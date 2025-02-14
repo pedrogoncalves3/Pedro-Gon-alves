3e4455b8e5e22fbeec8a5778eb3a1f591b9309d7
pedrogoncalves3 <pedrinhocabecao00@gmail.com>
2025-02-14 00:41:08 +0000

    salvando

---
+++ 3.atividades/1.atividade_salarios.py
@@ -0,0 +1,11 @@
+import os 
+os.system("clear")
+
+#calcule quantos salarios minimos uma pessoa recebe com base que o salario minimo e R$1350
+
+salario = float(input("digite seu salario: "))
+
+
+salario_minimo = 1350
+
+print(f"voçe ganha: {salario / salario_minimo} salarios minimos")
--- 3.atividades/2.priemira-atividade .py
+++ 3.atividades/2.priemira-atividade .py
@@ -3,17 +3,8 @@ os.system("clear")
 
 
 nome = input ("digite seu nome aqui: ")
-idade = int(input(digite sua idade: ))
+idade = int(input("digite a sua idade: "))
 numero = int(input("digite um numero: "))
-
-
-
-if numero < 10:
-        print ("acesso negado!")
-        print ("MENOR QUE 10! ")
-else:
-        print ("acesso permitido")
-        print ("MAIOR QUE 10! ")
 
 
 
